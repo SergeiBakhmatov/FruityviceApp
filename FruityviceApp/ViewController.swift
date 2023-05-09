0e6a30e7b9b4d1deb777ddcd3ab9235be88051f6
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-05-09 11:46:08 +0500

    Initial Commit

---
+++ FruityviceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FruityviceApp
+//
+//  Created by Sergei Bakhmatov on 09.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
