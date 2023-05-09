b0a629945a8bd2bdf41b72ae87dac5c3d7adb1e5
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-05-09 13:34:08 +0500

    Work with Storyboard, add IBOutlets labels.

--- FruityviceApp/ViewControllers/FruitsCollectionViewController.swift
+++ FruityviceApp/ViewControllers/FruitsCollectionViewController.swift
@@ -37,17 +37,17 @@ class FruitsCollectionViewController: UICollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return 1
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FruitCell", for: indexPath)
     
         // Configure the cell
     
---
+++ FruityviceApp/Views/FruitCell.swift
@@ -0,0 +1,17 @@
+//
+//  FruitCell.swift
+//  FruityviceApp
+//
+//  Created by Sergei Bakhmatov on 09.05.2023.
+//
+
+import UIKit
+
+final class FruitCell: UICollectionViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var familyLabel: UILabel!
+    @IBOutlet var genusLabel: UILabel!
+    @IBOutlet var orderLabel: UILabel!
+    
+    
+}
