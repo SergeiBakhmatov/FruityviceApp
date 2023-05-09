4f1ea7ade168d3349993a712b1e21aad018e7e3b
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-05-09 12:08:19 +0500

    Add model Fruit

---
+++ FruityviceApp/Models/Fruit.swift
@@ -0,0 +1,23 @@
+//
+//  Fruit.swift
+//  FruityviceApp
+//
+//  Created by Sergei Bakhmatov on 09.05.2023.
+//
+
+struct Fruit: Decodable {
+    
+    let name: String
+    let family: String
+    let genus: String
+    let order: String
+    let nutritions: Nutrition
+}
+
+struct Nutrition: Decodable {
+    let carbohydrates: Double
+    let protein: Double
+    let fat: Double
+    let calories: Double
+    let sugar: Double
+}
